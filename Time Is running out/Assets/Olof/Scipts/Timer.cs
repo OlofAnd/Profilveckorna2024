d6976d970c6f03064e8ad62d6c498d9051f664ce
OlofAnd <olof.andreasson@elev.ga.lbs.se>
2024-02-05 10:20:00 +0100

    klar med timer
    klar med timer för nu

--- Time Is running out/Assets/Olof/Scipts/Timer.cs
+++ Time Is running out/Assets/Olof/Scipts/Timer.cs
@@ -11,9 +11,33 @@ public class Timer : MonoBehaviour
 
     void Update()
     {
-        remainingTime -= Time.deltaTime;
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+            //Game OVER
+            timerText.color = Color.red;
+            //fixa så att timer blir röd när tiden är låg.
+        }
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
+
+
+
+    //public void Pause()
+    //{
+    //    pauseMenu.SetActive(true);
+    //    Time.timeScale = 0;
+    //}
+
+    //public void Resume()
+    //{
+    //    pauseMenu.SetActive(false);
+    //    Time.timeScale = !;
+    //}
 }
