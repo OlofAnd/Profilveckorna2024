38694e1ba499d295b74be04f734898d359a2a3f9
OskarAndren <oskar.andren@elev.ga.lbs.se>
2024-02-06 13:58:08 +0100

    Lagt till scoresystem med en hud som displayar

---
+++ Time Is running out/Assets/Oskar/Scripts/Hud/Score_script.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Score_script : MonoBehaviour
+{
+    [SerializeField] Player_Script player_Script;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] float amountScore;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        amountScore = player_Script.score;
+        scoreText.text = "Score: " + amountScore.ToString();
+    }
+}
