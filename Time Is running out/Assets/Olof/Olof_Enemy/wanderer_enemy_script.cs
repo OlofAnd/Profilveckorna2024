973022d027edf375bf60659551c26904ca6abc1f
OlofAnd <olof.andreasson@elev.ga.lbs.se>
2024-02-07 14:03:59 +0100

    Wanderer Klar
    klar med wanderer

--- Time Is running out/Assets/Olof/Olof_Enemy/wanderer_enemy_script.cs
+++ Time Is running out/Assets/Olof/Olof_Enemy/wanderer_enemy_script.cs
@@ -11,7 +11,7 @@ public class wanderer_enemy_script : Enemy_Abstract_Script
     GameObject target;
 
     Vector2 direction;
-    float speed;
+    [SerializeField] float speed;
     float angle;
 
     Random rng = new Random();
@@ -30,9 +30,10 @@ public class wanderer_enemy_script : Enemy_Abstract_Script
         rb = GetComponent<Rigidbody2D>();
     }
 
-    
+
     void Update()
     {
+        Debug.Log(playerDetected);
         EnemyBehaviour();
     }
     public override void EnemyBehaviour()
@@ -40,27 +41,75 @@ public class wanderer_enemy_script : Enemy_Abstract_Script
         // alla behaviour här a.k.a hur enemy ska hantera dess olika behaviour
 
         rb.velocity = direction * speed;
+        if (hurting)
+        {
+            hurtTimer = Time.time + 0.5f;
+            hurt();
+        }
+        else
+        {
+            if (!playerDetected)
+            {
+                Idle();
+            }
+            else if (Vector2.Distance(rb.position, target.transform.position) <= 7)
+            {
+                Walk();
+            }
+            else
+            {
+                Idle();
+            }
+        }
+        if (EnemyHealthPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    
+
     void Idle()
     {
         if (idleTimer < Time.time)
         {
-            
+            angle = rng.Next(0, 360);
+            angle = angle * math.PI / 180;
+            idleTimer = Time.time + rng.Next(2, 3);
+        }
+
+        direction = new Vector2(math.cos(angle), math.sin(angle));
+        if (idleTimer > Time.time + 1)
+        {
+            speed = 1;
+        }
+        else
+        {
+            speed = 0;
         }
     }
     void Walk()
     {
-        direction = ((Vector2)(target.transform.position) - rb.position) / Vector2.Distance(Vector2.zero, (Vector2)(target.transform.position) - rb.position);
+        direction = -((Vector2)(target.transform.position) - rb.position) / Vector2.Distance(Vector2.zero, (Vector2)(target.transform.position) - rb.position);
         speed = 3f;
     }
     void hurt()
     {
         EnemyHealthPoints--;
-        if(Time.time>hurtTimer)
+        if (Time.time > hurtTimer)
         {
             hurting = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.gameObject.tag == "Player")
+        {
+            playerDetected = true;
+            target = trig.gameObject;
+        }
+        if (trig.gameObject.tag == "Player_Bullet")
+        {
+            hurting = true;
         }
     }
 }
