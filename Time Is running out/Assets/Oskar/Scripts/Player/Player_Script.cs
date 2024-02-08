2b79dfd70765871e22fa0649025376bd4341eba6
OskarAndren <oskar.andren@elev.ga.lbs.se>
2024-02-08 10:43:06 +0100

    Tar damage av alla enemies med tag Enemy_bullet

--- Time Is running out/Assets/Oskar/Scripts/Player/Player_Script.cs
+++ Time Is running out/Assets/Oskar/Scripts/Player/Player_Script.cs
@@ -11,8 +11,8 @@ public class Player_Script : MonoBehaviour
 
 
     [Header("Health")]
-    [SerializeField] int maxHealth = 4;
-    [SerializeField] public int currentHealth;
+    [SerializeField] float maxHealth = 100;
+    [SerializeField] public float currentHealth;
 
     [Header("Score")]
     public int score = 0;
@@ -33,23 +33,45 @@ public class Player_Script : MonoBehaviour
         }
 
     }
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (currentHealth > 0 && other.gameObject.CompareTag("Enemy"))
-        {
-            //Gör så den tar skada av hur mycket damage enemyn gör
-            currentHealth -= enemy_test_script.damage;
-            Debug.Log("Skadad");
-            score += enemy_test_script.scoreGive;
+    //private void OnCollisionEnter2D(Collision2D other)
+    //{
+    //    if (currentHealth > 0 && other.gameObject.CompareTag("Enemy"))
+    //    {
+    //        //Gör så den tar skada av hur mycket damage enemyn gör
+    //        currentHealth -= enemy_test_script.damage;
+    //        Debug.Log("Skadad");
+    //        score += enemy_test_script.scoreGive;
 
+    //    }
+    //    Debug.Log("Collision");
+    //    if (other.gameObject.CompareTag("Enemy"))
+    //    {
+    //        Destroy(other.gameObject);
+    //        score += enemy_test_script.scoreGive;
+    //        game_controller_script.enemiesAlive--;
+    //        Debug.Log(game_controller_script.enemiesAlive);
+    //    }
+    //}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy_bullet"))
+        {
+            Debug.Log("Skadad");
+            MonoBehaviour[] scripts = other.gameObject.GetComponents<MonoBehaviour>();
+            foreach (MonoBehaviour script in scripts)
+            {
+                Debug.Log("Inne i foreach");
+                var enemyDamageFindProperty = script.GetType().GetProperty("Damage");
+                Debug.Log(enemyDamageFindProperty);
+                if (enemyDamageFindProperty != null)
+                {
+                    Debug.Log("Hittade damage: " + enemyDamageFindProperty);
+                    float damageValue = (float)enemyDamageFindProperty.GetValue(script);
+                    currentHealth -= damageValue;
+                    break;
+                }
+            }
         }
-        //Debug.Log("Collision");
-        //if (other.gameObject.CompareTag("Enemy"))
-        //{
-        //    Destroy(other.gameObject);
-        //    score += enemy_test_script.scoreGive;
-        //    game_controller_script.enemiesAlive--;
-        //    Debug.Log(game_controller_script.enemiesAlive);
-        //}
     }
 }
