b10b04473fc12ca1111b60a8a2a1579d7494c951
OlofAnd <olof.andreasson@elev.ga.lbs.se>
2024-02-08 11:11:59 +0100

    enemyDamage till Damage

--- Time Is running out/Assets/Olof/Olof_Enemy/mud_Enemy_Bullet_Script.cs
+++ Time Is running out/Assets/Olof/Olof_Enemy/mud_Enemy_Bullet_Script.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mud_Enemy_Bullet_Script : MonoBehaviour
+public class mud_Enemy_Bullet_Script : Bullet_Abstract_Script
 {
-    // Start is called before the first frame update
+    private GameObject Player;
+
     void Start()
     {
-        
+        Damage = 5f;
+        Speed = 6f;
+        FlyTime = 5f;
+        Target = "Player";
+        Player = GameObject.FindGameObjectWithTag("Player");
+        rb.GetComponent<Rigidbody2D>();
+        Direction = ((Vector2)Player.transform.position - rb.position).normalized;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        BulletMovement();    
     }
 }
