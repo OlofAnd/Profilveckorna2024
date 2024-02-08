a473c7981643f117fb807f5e47cdbe5a22ef6228
OlofAnd <olof.andreasson@elev.ga.lbs.se>
2024-02-08 14:22:42 +0100

    time powerup baserad på tid kvar
    time powerup baseras på hur mycket tid som är kvar när du kommer till upgrade room.
    Bytte också plats på card_left är nu höger kort men den har fortfarande card_display_script_left på sig

--- Time Is running out/Assets/Olof/Scipts/Card_Display_Script_Left.cs
+++ Time Is running out/Assets/Olof/Scipts/Card_Display_Script_Left.cs
@@ -6,6 +6,8 @@ using TMPro;
 
 public class Card_Display_Script_Left : MonoBehaviour
 {
+    [SerializeField] GameController_Script gameController;
+
     [Header("Ability decks")]
     [SerializeField] CardDeck deck_tier1;
     [SerializeField] CardDeck deck_tier2;
@@ -23,9 +25,27 @@ public class Card_Display_Script_Left : MonoBehaviour
 
     void Start()
     {
-        card_left = deck_time_tier1.cards[Random.Range(0,deck_time_tier1.cards.Count)]; // väljer ett random kort från deck_time_tier1
+        //card_left = deck_time_tier1.cards[Random.Range(0,deck_time_tier1.cards.Count)]; // väljer ett random kort från deck_time_tier1
 
         nameText.text = card_left.name; // ändra så att nameText är ett image
         artWorkImage.sprite = card_left.artwork;
+    }
+    private void Awake()
+    {
+        if (gameController.remainingTime <= 120f&&gameController.remainingTime>=60f)
+        {
+            card_left = deck_time_tier1.cards[Random.Range(0, deck_time_tier1.cards.Count)];
+            Debug.Log("1");
+        }
+        if (gameController.remainingTime <= 60f && gameController.remainingTime >= 30f)
+        {
+            card_left = deck_time_tier2.cards[Random.Range(0, deck_time_tier2.cards.Count)];
+            Debug.Log("2");
+        }
+        if (gameController.remainingTime <= 30f)
+        {
+            card_left = deck_time_tier3.cards[Random.Range(0, deck_time_tier3.cards.Count)];
+            Debug.Log("3");
+        }
     }
 }
