a73aed04f39b099773c46b3964d4df070defeb8f
OskarAndren <oskar.andren@elev.ga.lbs.se>
2024-02-09 10:33:09 +0100

    Freezing Aura added

---
+++ Time Is running out/Assets/Oskar/Scripts/Player/Abilities/FreezingAura_Script.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezingAura_Script : MonoBehaviour
+{
+    public bool isFreezingAuraActive;
+
+    [SerializeField] Card_Information_Script CardInfo_Script;
+    CircleCollider2D freezingAreaCollider;
+    Rigidbody2D otherRb;
+    SpriteRenderer sprRen;
+
+
+    void Start()
+    {
+        freezingAreaCollider = GetComponent<CircleCollider2D>();
+        sprRen = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+        isFreezingAuraActive = CardInfo_Script.freezingAura;
+        if (isFreezingAuraActive)
+        {
+            freezingAreaCollider.enabled = true;
+            sprRen.enabled = true;
+
+        }
+        else
+        {
+            freezingAreaCollider.enabled = false;
+            sprRen.enabled = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isFreezingAuraActive)
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                otherRb = other.GetComponent<Rigidbody2D>();
+                otherRb.constraints = RigidbodyConstraints2D.FreezePosition;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (isFreezingAuraActive)
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                otherRb = other.GetComponent<Rigidbody2D>();
+                otherRb.constraints = RigidbodyConstraints2D.None;
+                otherRb.freezeRotation = true;
+            }
+        }
+    }
+}
