2dfa7ffd57bad1e15cc2df457ad91022532a8ac0
OlofAnd <olof.andreasson@elev.ga.lbs.se>
2024-02-09 11:20:07 +0100

    börjat med att kolla vilka powerups som spelare valt

---
+++ Time Is running out/Assets/Olof/Powerup_check_script.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup_check_script : MonoBehaviour
+{
+    [SerializeField] Card_Information_Script cardInfo;
+
+    private void Update()
+    {
+        
+    }
+}
