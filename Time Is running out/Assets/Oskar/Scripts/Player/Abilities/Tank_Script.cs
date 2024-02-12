53969d68cc03452bcbb1f0e2d042ade6b8f06274
OskarAndren <oskar.andren@elev.ga.lbs.se>
2024-02-12 12:19:07 +0100

    abilty funk på spelare t3

---
+++ Time Is running out/Assets/Oskar/Scripts/Player/Abilities/Tank_Script.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank_Script : MonoBehaviour
+{
+    [SerializeField] Player_Script player_Script;
+
+
+    public bool isTankActive;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+
+    }
+}
