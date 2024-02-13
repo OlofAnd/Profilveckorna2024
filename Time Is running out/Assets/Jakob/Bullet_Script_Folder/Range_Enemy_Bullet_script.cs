16bdc984dc60660e93fbbf880bdf9b9f6f87e665
OskarAndren <123725447+OskarAndren@users.noreply.github.com>
2024-02-13 21:46:10 +0100

    Ranged enemy bullets kollar mot spelaren

--- Time Is running out/Assets/Jakob/Bullet_Script_Folder/Range_Enemy_Bullet_script.cs
+++ Time Is running out/Assets/Jakob/Bullet_Script_Folder/Range_Enemy_Bullet_script.cs
@@ -16,9 +16,9 @@ public class Range_Enemy_Bullet_script : Bullet_Abstract_Script
         rb = GetComponent<Rigidbody2D>();
         Direction = ((Vector2)Player.transform.position - rb.position).normalized;
 
-        //float rotation = Mathf.Atan2(-Direction.x, Direction.y) * Mathf.Rad2Deg;
-        //transform.position += (Vector3)Direction;
-        //transform.rotation = Quaternion.Euler(0, 0, rotation);
+        float rotation = Mathf.Atan2(-Direction.x, Direction.y) * Mathf.Rad2Deg;
+        transform.position += (Vector3)Direction;
+        transform.rotation = Quaternion.Euler(0, 0, rotation + 90);
     }
     void Update()
     {
