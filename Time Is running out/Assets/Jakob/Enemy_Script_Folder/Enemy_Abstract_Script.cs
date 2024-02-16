4965349636531c21f384168d51febc9d2b296f01
dr4k0b <120574803+dr4k0b@users.noreply.github.com>
2024-02-16 11:25:54 +0100

    unity crasha, nya animationer

--- Time Is running out/Assets/Jakob/Enemy_Script_Folder/Enemy_Abstract_Script.cs
+++ Time Is running out/Assets/Jakob/Enemy_Script_Folder/Enemy_Abstract_Script.cs
@@ -7,6 +7,13 @@ public abstract class Enemy_Abstract_Script : MonoBehaviour
     public float EnemyHealthPoints { get; set; }
     public float Damage { get; set; }
     public float ScoreValue { get; set; }
+    public int Wave
+    {
+        get
+        {
+            return FindObjectOfType<GameController_Script>().Wave;
+        }
+    }
     public int PlayerDamage
     {
         get
